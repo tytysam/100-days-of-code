41ecbc312bc7a2f5a331c6d2cff25614c56ad43e
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-08 16:17:30 -0700

    Built DayTemplate component

---
+++ 100-days-of-code-logger/src/components/DayTemplate.js
@@ -0,0 +1,140 @@
+import React from "react"
+import moment from "moment"
+import Img from "gatsby-image"
+import { Link } from "gatsby"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import { Button, Card, Typography, useMediaQuery } from "@material-ui/core"
+import Layout from "../components/layout.js"
+import SEO from "../components/seo.js"
+import Share from "../components/Share.js"
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    maxWidth: 700,
+    position: "relative",
+  },
+  background: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  article: {
+    margin: "48px 0px",
+    overflow: "hidden",
+    "& img": {
+      maxWidth: "100%",
+    },
+    wordBreak: "break-word", // this should prevent the words in my title from causing page to split
+  },
+  text: {
+    margin: theme.spacing(2, 0),
+    " & p > a, li > a, h3 > a, h2 > a": {
+      borderBottom: `2px solid ${theme.palette.primary.main}`,
+    },
+    lineHeight: 1.8,
+    padding: "16px 16px 0px 16px",
+  },
+  typography: {
+    padding: "0px 16px 16px 16px",
+  },
+  share: {
+    padding: "0px 16px 16px 16px",
+  },
+}))
+
+const DayTemplate = ({ data, pageContext }) => {
+  const classes = useStyles()
+  const theme = useTheme()
+  const isTablet = useMediaQuery(theme.breakpoints.down("sm"))
+
+  const {
+    title,
+    publishDate,
+    shareText,
+    hashtags,
+    thumbnailImage,
+  } = data.markdownRemark.frontmatter
+  return (
+    <Layout>
+      <SEO
+        title={`${data.site.siteMetadata.title} | ${title}`}
+        description={data.site.siteMetadata.description}
+        url={typeof window !== "undefined" ? window.location.href : ""}
+        image={thumbnailImage.childImageSharp.fluid.src}
+      />
+      <div className={classes.background}>
+        <div className={classes.container}>
+          <Card className={classes.article}>
+            <Typography
+              variant="h1"
+              style={{ fontSize: isTablet ? "2rem" : "2.5rem" }}
+              className={classes.typography}
+            >
+              {data.markdownRemark.frontmatter.title}
+            </Typography>
+            <div className={classes.share}>
+              <Share title={title} text={shareText} hashtags={hashtags} />
+            </div>
+            <Typography variant="subtitle2" className={classes.typography}>
+              {moment(publishDate).format("MMMM Do, YYYY")}
+            </Typography>
+            <Img
+              fluid={thumbnailImage.childImageSharp.fluid}
+              alt={pageContext.title}
+            />
+            <Typography
+              variant="body1"
+              className={classes.text}
+              component="div"
+              dangerouslySetInnerHTML={{
+                __html: data.markdownRemark.html,
+              }}
+            />
+          </Card>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginBottom: 48,
+            }}
+          >
+            <Link to="/">
+              <Button variant="text" color="default" size="large">
+                Back to home
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default DayTemplate
+
+export const dayQuery = graphql`
+  query dayQuery($day: String!) {
+    markdownRemark(frontmatter: { day: { eq: $day } }) {
+      frontmatter {
+        title
+        day
+        publishDate
+        shareText
+        hashtags
+        thumbnailImage {
+          childImageSharp {
+            fluid(maxWidth: 400, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      html
+    }
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
