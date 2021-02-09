4f36907ba4045e9e253dabbefd0cd2691ded996a
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-08 18:07:11 -0700

    Changes to content on 404

--- 100-days-of-code-logger/src/pages/404.js
+++ 100-days-of-code-logger/src/pages/404.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout.js"
+import SEO from "../components/seo.js"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <h1>404 Error: Page not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
