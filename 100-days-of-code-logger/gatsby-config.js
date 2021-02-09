423322b8dbe7013b138b5f3bcaec4a4f259e0e3c
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-08 18:44:33 -0700

    Data updates to gatsby-config.js

--- 100-days-of-code-logger/gatsby-config.js
+++ 100-days-of-code-logger/gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tyler's 100 Days of Code`,
+    description: `Hi, I'm Tyler! I'm a digital creator + developer completing R1 of the 100 Days of Code Challenge.`,
+    author: `@tytysam`,
+    githubUrl: "https://github.com/tytysam",
+    twitterUrl: "https://twitter.com/TyTySam",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,18 +15,48 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `100 Days of Code Template`,
+        short_name: `template`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/TCS_main.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
