123a927de313bd859855fc3c140ed5729d1067bc
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-08 17:50:43 -0700

    added theming and formatting to header.js

--- 100-days-of-code-logger/src/components/header.js
+++ 100-days-of-code-logger/src/components/header.js
@@ -1,42 +1,73 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { makeStyles } from "@material-ui/core/styles"
+import { Avatar, Button, Grid, Typography } from "@material-ui/core"
+import { Twitter } from "@material-ui/icons"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const useStyles = makeStyles(theme => ({
+  avatarContainer: {
+    display: "flex",
+    flexDirection: "row",
+    maxWidth: 600,
+  },
+  avatar: {
+    width: theme.spacing(6),
+    height: theme.spacing(6),
+    marginRight: theme.spacing(2),
+  },
+}))
+
+const Header = ({ avatar, siteDescription, twitterUrl }) => {
+  const classes = useStyles()
+
+  return (
+    <header>
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <Grid container direction="row" spacing={2} justify="space-between">
+          <Grid item>
+            <div className={classes.avatarContainer}>
+              <Link to={"/"}>
+                <Avatar
+                  src={avatar.childImageSharp.fixed.src}
+                  alt="TCS Personal Wordmark :)"
+                  className={classes.avatar}
+                />
+              </Link>
+              <Typography gutterBottom variant="subtitle2">
+                {siteDescription}
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item>
+            <Button
+              variant="text"
+              color="default"
+              href={twitterUrl}
+              target="_blank"
+              startIcon={<Twitter />}
+            >
+              Follow me on Twitter
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  siteDescription: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteDescription: ``,
 }
 
 export default Header
